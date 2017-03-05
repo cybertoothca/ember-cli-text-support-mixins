6318d5b1d74ca7a2b9e8049f307e00e87a150566
Dan Nelson <dnelson@cybertooth.ca>
2017-03-05 00:22:39 -0700

    #2 Basic text-area component uses all the mixins.

--- addon/mixins/ctrl-enter-submits-form.js
+++ addon/mixins/ctrl-enter-submits-form.js
@@ -50,7 +50,6 @@ export default Ember.Mixin.create({
   /**
    * Grab the nearest form.  Normally you can just ask an input for it's `this.form`, however that doesn't
    * seem to work with Ember wrapped TextSupport components?  Using a jQuery find of the closest form instead.
-   * This `this.$('input')[0].form` should actually work across all browsers.
    */
   _form: Ember.computed(function () {
     return this.$().closest('form');
