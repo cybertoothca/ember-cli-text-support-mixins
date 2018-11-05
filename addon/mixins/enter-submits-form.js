6c967bbc3f0de21b3487585e86bfb0c068250179
Dan Nelson <dnelson@cybertooth.ca>
2018-11-05 12:37:50 -0700

    #16 More complexity fixes for code climate

--- addon/mixins/enter-submits-form.js
+++ addon/mixins/enter-submits-form.js
@@ -8,8 +8,7 @@ export default Mixin.create(ClosestFormMixin, {
 
   keyDown(event) {
     this._super(...arguments);
-    if ((!event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey) &&
-      (event.keyCode === KeyEvent.DOM_VK_ENTER || event.keyCode === KeyEvent.DOM_VK_RETURN)) {
+    if (this._notAltCtrlMetaShift(event) && this._isEnterOrReturnKey(event)) {
       event.preventDefault();
       const form = this.get('_form');
       if (isPresent(form) && this.get('enterWillSubmitForm?')) {
@@ -19,5 +18,23 @@ export default Mixin.create(ClosestFormMixin, {
       return false;
     }
     return true;
+  },
+
+  /**
+   * @param event the event to test
+   * @return {boolean} true if the enter OR the return key was pressed, false otherwise
+   * @private
+   */
+  _isEnterOrReturnKey(event) {
+    return event.keyCode === KeyEvent.DOM_VK_ENTER || event.keyCode === KeyEvent.DOM_VK_RETURN;
+  },
+
+  /**
+   * @param event the event to test
+   * @return {boolean} true if none of the modifier keys are pressed (alt, ctrl, meta, and shift), false otherwise
+   * @private
+   */
+  _notAltCtrlMetaShift(event) {
+    return !event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey;
   }
 });
