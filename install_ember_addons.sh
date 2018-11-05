1e31aafad5c702e96037aa2682f8afb712f4d3aa
Dan Nelson <dnelson@cybertooth.ca>
2018-11-05 13:02:12 -0700

    #16 Trying to add code coverage

--- install_ember_addons.sh
+++ install_ember_addons.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 ember install ember-cli-bootstrap3-sass
+ember install ember-cli-code-coverage
 ember install ember-cli-deploy
 ember install ember-cli-deploy-build
 ember install ember-cli-deploy-gzip
 ember install ember-cli-deploy-s3
 ember install ember-cli-font-awesome-sass
-ember install ember-cli-font-awesome-sass
 ember install ember-route-action-helper
-ember install ember-cli-node-assets
 
 yarn add bootswatch@3.3 --dev
 yarn add keyevent --dev
