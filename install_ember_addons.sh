ff334b2d3299c9e910bcb2f723a523e8acaaea20
Dan Nelson <dan@cybertooth.io>
2018-11-09 17:53:44 -0700

    #23 Copy install command to clipboard

--- install_ember_addons.sh
+++ install_ember_addons.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+yarn add clipboard --dev
+
 ember install @fortawesome/ember-fontawesome
 yarn add @fortawesome/free-solid-svg-icons --dev
 yarn add @fortawesome/free-brands-svg-icons --dev
 
-ember install ember-cli-element-closest-polyfill
+ember install ember-auto-import
+
+ember install ember-cli-app-version
 
 ember install ember-cli-bootstrap-4
+
 ember install ember-cli-deploy
 ember install ember-cli-deploy-build
 ember install ember-cli-deploy-gzip
 ember install ember-cli-deploy-s3
+
+ember install ember-cli-element-closest-polyfill
 
 ember install ember-route-action-helper
 
